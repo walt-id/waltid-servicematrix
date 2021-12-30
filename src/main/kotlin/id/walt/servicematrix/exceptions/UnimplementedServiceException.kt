58f2d5cbe596d5f548ccc5473c3e82c8764df894
waltkb <kevin@walt.id>
2021-12-31 00:11:19 +0100

    Make ServiceMatrix UnimplementedServiceException error message more descriptive

--- src/main/kotlin/id/walt/servicematrix/exceptions/UnimplementedServiceException.kt
+++ src/main/kotlin/id/walt/servicematrix/exceptions/UnimplementedServiceException.kt
@@ -1,4 +1,4 @@
 package id.walt.servicematrix.exceptions
 
 class UnimplementedServiceException(service: String?) :
-    Exception("No implementation has been registered for service: $service")
+    Exception("walt.id ServiceMatrix: No implementation has been registered for service: \"$service\"! This is a configuration error. Information about the ServiceMatrix: https://github.com/walt-id/waltid-ssikit/blob/master/README.md")
