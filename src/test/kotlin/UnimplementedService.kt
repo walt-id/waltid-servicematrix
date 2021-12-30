b7cd3360bb52b64d6a3f336f9436bd4cc977fdbd
waltkb <kevin@walt.id>
2021-12-31 00:11:48 +0100

    Check for class name in UnimplementedServiceException in UnimplementedServiceTest

--- src/test/kotlin/UnimplementedService.kt
+++ src/test/kotlin/UnimplementedService.kt
@@ -1,16 +1,18 @@
 import id.walt.servicematrix.BaseService
 import id.walt.servicematrix.ServiceProvider
-import id.walt.servicematrix.ServiceRegistry
 import id.walt.servicematrix.exceptions.UnimplementedServiceException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.string.shouldContain
 
 class UnimplementedServiceTest : StringSpec({
     "Service without implementation should throw UnimplementedServiceException" {
-        shouldThrow<UnimplementedServiceException> {
+        val exception = shouldThrow<UnimplementedServiceException> {
             val theService = UnimplementedService.getService()
             theService.function1()
         }
+        exception.message shouldContain "UnimplementedService" // Class name
+        println(exception.message)
     }
 })
 
